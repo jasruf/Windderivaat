c58803806aa6dcc3b97370877ebfa29ec151c7f6
Tony <Tony@130.013.092.145.hva.nl>
2013-03-11 23:23:29 +0100

    Comments added and debugging lines commented.

--- src/chart/Data.java
+++ src/chart/Data.java
@@ -24,31 +24,33 @@ import java.util.logging.Logger;
  * @author Tony
  */
 public class Data {
-    public static void main(String[] args) {
-        Data data = new Data("/Users/Tony/Dropbox/Prove IT/107 - Simulatie Windderivaten/test_data_wind.txt");
-        try {
-            data.fill();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+
     
-            
-    private final int skipLines;
-    private List<Calendar> dateArray = new ArrayList<Calendar>();
-    private List<Integer> windSpeedArray = new ArrayList<Integer>();
-    private String data;
-    private String fileName;
-    private File file;
+    // This main was added for testing only
+//    public static void main(String[] args) { 
+//        Data data = new Data("/Users/Tony/Dropbox/Prove IT/107 - Simulatie Windderivaten/test_data_wind.txt");//In the arguments give the path of the data documents 
+//        try {
+//            data.fill();
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+    private final int skipLines;// How many line to skip 
+    private List<Calendar> dateArray = new ArrayList<Calendar>();// ArrayList for the date 
+    private List<Integer> windSpeedArray = new ArrayList<Integer>();//ArrayList for the wind speed 
+    private String data;// String is being used to pick up data from the data documents and is use to fill the array 
+    private String fileName;// String being used to tell the program where the data documents is
+    private File file;//File is the fileName 
     private FileInputStream fis;
     private DataInputStream dis;
     private BufferedReader br;
     private SimpleDateFormat dateFormat;
-
-    public Data(String filename) {
-        skipLines = 22;
+    
+    
+    public Data(String filename) {//method used to get data ready for use 
+        skipLines = 22;//
         dateArray = new ArrayList<Calendar>();
         windSpeedArray = new ArrayList<Integer>();
         data = null;
@@ -61,9 +63,9 @@ public class Data {
         }
         dis = new DataInputStream(fis);
         br = new BufferedReader(new InputStreamReader(dis));
-        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH");
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH");//SimpleDateFormat is used to show the date as a specific format
     }
-    
+
     public List<Calendar> getDateArray() {
         return dateArray;
     }
@@ -74,29 +76,26 @@ public class Data {
 
     public void fill() throws FileNotFoundException, IOException {
         Calendar date;
-        if (!file.exists()) {
+        if (!file.exists()) {// Error prompt that the file is not found 
             System.out.println("I can't find the file: " + fileName);
         }
 
         for (int i = 0; i < skipLines; i++) {
-            br.readLine();
+            br.readLine();// Skip the lines that we don't need "lines with no data"
         }
-        while (((data = br.readLine()) != null)) {
+        while (((data = br.readLine()) != null)) {//Whille we can still get data add it to both arraylists "the date and wind speed"
+            //Set the date with date valuetype as a GergorinCalender and add the year, month, day, hour and the minutes are set to 0 "0 because we dont have the minutes and dont need them"
             date = new GregorianCalendar(Integer.parseInt(data.substring(0, 4)), (Integer.parseInt(data.substring(4, 6)) - 1), Integer.parseInt(data.substring(6, 8)), Integer.parseInt(data.substring(9, 11)), 0);
-
-            dateArray.add(date);
-            windSpeedArray.add(Integer.parseInt(data.substring(20, 23).replaceAll("\\s", "")));
-            //System.out.println(dateFormat.format((dateArray.get(dateArray.size()-1)).getTime())+ " " + windSpeedArray.get(windSpeedArray.size()-1));
-            //System.out.println(dateFormat.format(date.getTime()));
+            dateArray.add(date);// add the date to the ArrayList and replace 
+            windSpeedArray.add(Integer.parseInt(data.substring(20, 23).replaceAll("\\s", "")));//Add the wind speed to the ArrayList and replaces any spaces with nothing
+            
+            //System.out.println(dateFormat.format(date.getTime()));//Testing output just after filling the first data into the array 
         }
-        for (int i = 0; i < windSpeedArray.size(); i++) {
-            System.out.println(dateFormat.format(dateArray.get(i).getTime()) + " " + windSpeedArray.get(i));
-        }
+//        for (int i = 0; i < windSpeedArray.size(); i++) {// print after the list has been tottaly filled 
+//            System.out.println(dateFormat.format(dateArray.get(i).getTime()) + " " + windSpeedArray.get(i));
+//        }
         br.close();
         dis.close();
         fis.close();
     }
-
-
-    
 }
