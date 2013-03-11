90046f7afdcce9f08d95ab9cf76607d63efb2619
Loki <>
2013-03-11 11:21:48 +0100

    Test 1234

--- src/chart/ChartTest.java
+++ src/chart/ChartTest.java
@@ -21,6 +21,8 @@ import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.statistics.HistogramType;
 import org.jfree.data.xy.IntervalXYDataset;
 import org.jfree.data.xy.WindDataset;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -42,11 +44,10 @@ public class ChartTest extends JFrame {
 
         //jLayer.setB
         // This will create the dataset 
-        IntervalXYDataset dataset = createDataset();
+        CategoryDataset dataset = createCategoryDataset();
         
         // based on the dataset we create the chart
-        JFreeChart chart = ChartFactory.createHistogram(
-                "Shit", "Supershit", "megaShit", dataset, PlotOrientation.VERTICAL, true, false, false);
+        JFreeChart chart = ChartFactory.createLineChart("", "", "", dataset, PlotOrientation.VERTICAL, false, false, false);
         // we put the chart into a panel
         
         ChartPanel chartPanel = new ChartPanel(chart);
@@ -73,8 +74,55 @@ public class ChartTest extends JFrame {
      * Creates a sample dataset 
      */
 
-    private  HistogramDataset createDataset() {
-        return createHistogramDataset();
+    private CategoryDataset createCategoryDataset() {
+        
+        // row keys...
+        final String series1 = "First";
+        final String series2 = "Second";
+        final String series3 = "Third";
+
+        // column keys...
+        final String type1 = "Type 1";
+        final String type2 = "Type 2";
+        final String type3 = "Type 3";
+        final String type4 = "Type 4";
+        final String type5 = "Type 5";
+        final String type6 = "Type 6";
+        final String type7 = "Type 7";
+        final String type8 = "Type 8";
+
+        // create the dataset...
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(1.0, series1, type1);
+        dataset.addValue(4.0, series1, type2);
+        dataset.addValue(3.0, series1, type3);
+        dataset.addValue(5.0, series1, type4);
+        dataset.addValue(5.0, series1, type5);
+        dataset.addValue(7.0, series1, type6);
+        dataset.addValue(7.0, series1, type7);
+        dataset.addValue(8.0, series1, type8);
+
+        dataset.addValue(5.0, series2, type1);
+        dataset.addValue(7.0, series2, type2);
+        dataset.addValue(6.0, series2, type3);
+        dataset.addValue(8.0, series2, type4);
+        dataset.addValue(4.0, series2, type5);
+        dataset.addValue(4.0, series2, type6);
+        dataset.addValue(2.0, series2, type7);
+        dataset.addValue(1.0, series2, type8);
+
+        dataset.addValue(4.0, series3, type1);
+        dataset.addValue(3.0, series3, type2);
+        dataset.addValue(2.0, series3, type3);
+        dataset.addValue(3.0, series3, type4);
+        dataset.addValue(6.0, series3, type5);
+        dataset.addValue(3.0, series3, type6);
+        dataset.addValue(4.0, series3, type7);
+        dataset.addValue(3.0, series3, type8);
+
+        return dataset;
+                
     }
     
     private HistogramDataset createHistogramDataset() {
--- src/chart/ChartViewer.java
+++ src/chart/ChartViewer.java
@@ -8,7 +8,11 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.XYDataset;
 
 /**
@@ -24,11 +28,22 @@ public class ChartViewer extends JPanel {
     private ChartPanel avgWindspeed;
     
     private JLayeredPane jLayer;
-    ChartViewer(XYDataset polarData ) {
+    ChartViewer(XYDataset polarData, HistogramDataset histogramData, CategoryDataset sumWindspeedWeekData) {
         jLayer = new JLayeredPane();
         
         this.setLayout(new BorderLayout());
         this.add(jLayer, BorderLayout.CENTER);
+        
+        JFreeChart polar = ChartFactory.createPolarChart("", polarData, true, true, true);
+        
+        
+        
+        jLayer.add(polarChart);
+        jLayer.add(avgWindspeed);
+        jLayer.add(histogram);
+        jLayer.add(sumWindspeedQuarter);
+        jLayer.add(sumWindspeedMonth);
+        jLayer.add(sumWindspeedWeek);
         
         polarChart.setBounds(0, 0, 500, 600);
         avgWindspeed.setBounds(0, 0, 500, 600);
