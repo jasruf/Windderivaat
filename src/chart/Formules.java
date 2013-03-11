5315ad4be97d1723d1bd320c230b4f65921f0d3e
Tony <Tony@130.013.092.145.hva.nl>
2013-03-11 19:34:23 +0100

    parseData deleted and in data implemented

--- src/chart/Formules.java
+++ src/chart/Formules.java
@@ -4,46 +4,63 @@
  */
 package chart;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author JASEEN
  */
 public class Formules {
 
-    Data data[];
-    double avgDay=0;
-    double avgMonth=0;
-    double avgYear=0;
+    Data data = new Data("/Users/Tony/Dropbox/Prove IT/107 - Simulatie Windderivaten/test_data_wind.txt");
+    double avgDay = 0;
+    double avgMonth = 0;
+    double avgYear = 0;
 
-    public int avgDay() {
-       
-        for (int i = 0; i < data.length; i++) {
-
-            for (int j = 0; j <= 23; j++) {
-
-                avgDay = +data[i].speed;
-            }
+    public Formules() {
+        try {
+            data.fill();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Formules.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Formules.class.getName()).log(Level.SEVERE, null, ex);
         }
-        avgDay = avgDay / 24;
-        return 0;
+        
+        
     }
 
-    public int avgMonth() {
-
-        for (int i = 0; i < 31; i++) {
-
-            avgMonth = +avgDay;
-
-        }
-        avgMonth /= 31;
-        return 0;
-    }
-
-    public double avgYear() {
-        for (int i = 0; i < 365; i++) {
-            avgYear = +data[i].speed;
-        }
-        avgYear /= 365;
-        return 0;
-    }
+//    public int avgDay() {
+//
+//        for (int i = 0; i <.; i++) {
+//
+//            for (int j = 0; j <= 23; j++) {
+//
+//                avgDay = +data[i].speed;
+//            }
+//        }
+//        avgDay = avgDay / 24;
+//        return 0;
+//    }
+//
+//    public int avgMonth() {
+//
+//        for (int i = 0; i < 31; i++) {
+//
+//            avgMonth += avgDay;
+//
+//        }
+//        avgMonth /= 31;
+//        return 0;
+//    }
+//
+//    public double avgYear() {
+//        for (int i = 0; i < 365; i++) {
+//            avgYear = +data[i].speed;
+//        }
+//        avgYear /= 365;
+//        return 0;
+//    }
 }
