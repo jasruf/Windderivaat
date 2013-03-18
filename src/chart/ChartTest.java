b352df16654ca47e3200f8096b894c044be9a132
Tony <Tony@130.013.092.145.hva.nl>
2013-03-18 14:07:41 +0100

    Formula: computeAvgDay complete computeAvgMonth in progress (not finished )

--- src/chart/ChartTest.java
+++ src/chart/ChartTest.java
@@ -6,8 +6,13 @@ package chart;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
+import math.Formulas;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
@@ -58,10 +63,23 @@ public class ChartTest extends JFrame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ChartTest demo = new ChartTest("Comparison", "Which operating system are you using?");
-        demo.pack();
-        demo.setVisible(true);
+        
+        Data data = null;
+        try {
+            data = new Data("/Users/Tony/Dropbox/Prove IT/107 - Simulatie Windderivaten/test_data_wind.txt");
+            data.fill();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Formulas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Formulas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        Formulas obj = new Formulas(data, 1993, 2001);
+        obj.computeAvgDay();
+        obj.computeAvgMonth();
     }
+    
     
     /**
      * Creates a sample dataset 
